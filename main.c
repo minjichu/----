6ec1f24ba965d0031f8bd9d9c4e8543a6b249993
minjichu <key0876@naver.com>
2018-11-22 15:44:33 +0900

    02

--- main.c
+++ main.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-
-	char src[100]="Programming course";
-	char dst[100];
-	int i=0;
-	while (src[i] != '\0')
+	FILE*fp;
+	char input[100];
+	int i;
+	fp = fopen ("sample.txt","w");
+	for (i=0;i<3;i++)
 	{
-		dst[i]=src[i];
-		i++;
-		}
-		dst[i]='\0';	/* 문자열 끝나는걸 넣어줌 */ 
-	printf (" dst : %s\n " , dst) ;
-	
+		printf("input a word :");
+		scanf("%s",input);
+		fprintf(fp, "%s\n", input);
+	}
+	fclose(fp);
 	
 	return 0;
 }
